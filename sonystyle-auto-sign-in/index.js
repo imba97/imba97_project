db8b56ce9638b67824c7b1b08b95fdbf762ca6c1
imba97 <mail@imba97.cn>
2021-06-13 11:16:52 +0800

    新增索尼社区签到

--- sonystyle-auto-sign-in/index.js
+++ sonystyle-auto-sign-in/index.js
@@ -14,6 +14,7 @@ cron.schedule('5 0 0 * * *', () => {
 
 // 执行签到
 function doSignin() {
+  let timeout = 3;
   // 登录
   axios
     .post(
@@ -25,33 +26,53 @@ function doSignin() {
       },
       {
         headers: {
-          'Content-Type': 'application/json;charset=UTF-8'
+          'Content-Type': 'application/json;charset=UTF-8',
+          'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
         }
       }
     )
-    .then((response) => {
+    .then(async (response) => {
       if (response.data.resultMsg[0].code !== '00') {
-        console.error(
-          `[error] 登陆失败: ${response.data.resultMsg[0].message}`
-        );
+        console.log(`[error] 登陆失败: ${response.data.resultMsg[0].message}`);
+        if (timeout-- > 0) doSignin();
         return;
       }
       // 拿到 access token
       const access_token = response.data.resultData.access_token;
 
-      // 签到
-      axios
+      // 主站签到
+      await axios
         .post(
           `https://www.sonystyle.com.cn/eSolverOmniChannel/account/signupPoints.do?access_token=${access_token}`,
           null,
           {
             headers: {
-              'Content-Type': 'application/json'
+              'Content-Type': 'application/json',
+              'User-Agent':
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
             }
           }
         )
         .then((res) => {
-          console.log(res.data.resultMsg[0].message);
+          console.log(`主站签到：${res.data.resultMsg[0].message}`);
+        });
+
+      // 社区签到
+      axios
+        .post(
+          'https://www.sonystyle.com.cn/mysony/bbsapp/index.php?app=user&ac=api&api=user&ts=signin',
+          {
+            channel: 'web'
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${access_token}`
+            }
+          }
+        )
+        .then((response) => {
+          console.log(`社区签到：${response.data.msg}`);
         });
     });
 }
