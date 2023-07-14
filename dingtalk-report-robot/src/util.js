b28037f981ebd14b6c291d1050598889aa7b91a3
imba97 <mail@imba97.cn>
2023-07-14 11:50:15 +0800

    feat: 支持查询指定日期

--- dingtalk-report-robot/src/util.js
+++ dingtalk-report-robot/src/util.js
@@ -4,16 +4,10 @@ const { execSync } = require('child_process')
 
 const config = require('./config')
 
-/**
- * 是否是指定作者
- * @param {string} line 当前行 要匹配的字符串
- * @param {string} name 用户名
- * @param {string} mail 用户邮箱
- * @returns
- */
-function isAuthor(line, name, mail) {
-  return new RegExp(`Author: ${name} <${mail}>`).test(line)
-}
+const after = _.get(process.argv, '2', config.after)
+const before = _.get(process.argv, '3', config.after)
+
+const isDate = /\d{4}-\d{2}-\d{2}/.test(after)
 
 module.exports = {
   /**
@@ -34,14 +28,17 @@ module.exports = {
       console.log(e)
     }
 
-    const res = execSync(
-      `git log --after="${moment()
-        .subtract(_.get(process.argv, '2', config.after), 'days')
-        .format('YYYY-MM-DD 00:00:00')}"`,
-      {
-        cwd: path
-      }
-    )
+    let command = `git log --author="${user.name}" `
+
+    if (isDate) {
+      command += `--after="${after} 00:00:00" --before="${before || after} 23:59:59"`
+    } else {
+      command += `--after="${moment().subtract(after, 'days').format('YYYY-MM-DD 00:00:00')}"`
+    }
+
+    const res = execSync(command, {
+      cwd: path
+    })
 
     const line = res
       .toString()
@@ -50,23 +47,8 @@ module.exports = {
 
     const commit = []
 
-    let isUser = false
-
     line.forEach((item) => {
-      if (/^commit/.test(item)) {
-        isUser = false
-        return
-      }
-
-      if (/^Author/.test(item)) {
-        isUser = isAuthor(item, user.name, user.mail)
-      }
-
-      if (
-        (user.prefix !== null && !user.prefix.test(item)) ||
-        /^(Merge|Author|Date)/.test(item) ||
-        !isUser
-      ) {
+      if ((user.prefix !== null && !user.prefix.test(item)) || /^(Merge|Author|Date)/.test(item)) {
         return
       }
 
@@ -79,7 +61,7 @@ module.exports = {
 
     if (commit.length > 0) {
       result += `${key}\n`
-      result += commit.map((item) => `● ${item}`).join('\n')
+      result += commit.reverse().map((item, index) => `${index + 1}. ${item}`).join('\n')
       result += '\n'
     }
 
